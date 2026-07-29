e8a66b2e3d54e0b0b9d2e3689b84ca38ba663e0c
Valhiente <robertovalhiente@gmail.com>
2026-07-29 09:41:14 -0300

    Add src/app/carrinho/page.tsx for commerce platform

---
+++ src/app/carrinho/page.tsx
@@ -0,0 +1,7 @@
+import { CartView } from "@/components/CartView";
+
+export const metadata = { title: "Sacola" };
+
+export default function CartPage() {
+  return <main className="cart-page"><CartView /></main>;
+}
